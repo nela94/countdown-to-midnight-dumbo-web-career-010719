e40efab6031ee70a479f4767909ccef9f799e8e3
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-12-28 17:54:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
 def countdown(number)
-  number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
   number -= 1
 end
+"HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(sleep)
