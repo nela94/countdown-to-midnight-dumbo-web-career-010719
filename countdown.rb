20f65511bebe49b8c965e5782b9beadeddea6f6e
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-12-28 17:51:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
   number -= 1
-else 
+else
   return "HAPPY NEW YEAR!"
 end
 end
@@ -13,4 +13,3 @@ end
 def countdown_with_sleep(sleep)
   sleep(5)
 end
-
